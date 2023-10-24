a7342997ea01e6cafd1c18c2e06e9f2642a13726
Ian Ozsvald <ian@ianozsvald.com>
2023-10-24 15:54:02 +0100

    added new fn, added tests, ran black

--- simpler_pandas.py
+++ simpler_pandas.py
@@ -2,7 +2,6 @@ from functools import partial
 import pandas as pd
 import numpy as np
 from IPython.display import display
-import pytest
 from labelling import format_to_base_10
 
 # make_bin_edges will turn e.g. "1 2 ... 10" into [-inf, 1, 2, ..., 9, 10, inf]
